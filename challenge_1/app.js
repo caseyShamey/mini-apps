84b35a91cbbaf821592bd7df3727db4b8f1b365c
Casey Shamey <caseyshamey@gmail.com>
2018-11-20 11:21:44 -0500

    Complete board setup and turn tracking

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,4 +1,29 @@
-document.getElementById("one").addEventListener("click", () => {
-  document.getElementById("one").innerHTML = "X"
-});
+var turnTracker = true;
+var currentBoard = {}
 
+var clicker = (id) => {
+  // if (
+  //   (currentBoard.one === "X" &&
+  //   currentBoard.two === "X" &&
+  //   currentBoard.three === "X") ||
+
+  //   (currentBoard.one === "O" &&
+  //   currentBoard.two === "O" &&
+  //   currentBoard.three === "O") ||
+
+  //   (currentBoard.one === "X" &&
+  //   currentBoard.two === "X" &&
+  //   currentBoard.three === "X") ||
+  // )
+  if (turnTracker === true) {
+    document.getElementById(id).innerHTML = "X"
+    currentBoard[id] = "X"
+    turnTracker = false
+    document.getElementById("turn").innerHTML = "Player Turn: O"
+  } else {
+    document.getElementById(id).innerHTML = "O"
+    currentBoard[id] = "O"
+    turnTracker = true
+    document.getElementById("turn").innerHTML = "Player Turn: X"
+  }
+}
