2e0b3cec4b5e0c83c2982b1d6d38d1a0db9368b1
Casey Shamey <caseyshamey@gmail.com>
2018-11-20 11:55:48 -0500

    Complete implmentation of rules and win

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,29 +1,64 @@
-var turnTracker = true;
+var turnTracker = "X";
 var currentBoard = {}
+var win = false
 
 var clicker = (id) => {
-  // if (
-  //   (currentBoard.one === "X" &&
-  //   currentBoard.two === "X" &&
-  //   currentBoard.three === "X") ||
-
-  //   (currentBoard.one === "O" &&
-  //   currentBoard.two === "O" &&
-  //   currentBoard.three === "O") ||
-
-  //   (currentBoard.one === "X" &&
-  //   currentBoard.two === "X" &&
-  //   currentBoard.three === "X") ||
-  // )
-  if (turnTracker === true) {
+  if (currentBoard[id] === "X" || currentBoard[id] === "O") {
+    //can't click on same square twice during game
+  } else if (win) {
+    //can't make moves after win
+  } else if (turnTracker === "X") {
     document.getElementById(id).innerHTML = "X"
     currentBoard[id] = "X"
-    turnTracker = false
+    winChecker(turnTracker)
+    turnTracker = "O"
     document.getElementById("turn").innerHTML = "Player Turn: O"
   } else {
     document.getElementById(id).innerHTML = "O"
     currentBoard[id] = "O"
-    turnTracker = true
+    winChecker(turnTracker)
+    turnTracker = "X"
     document.getElementById("turn").innerHTML = "Player Turn: X"
+  }
+}
+
+var winChecker = (turnTracker) => {
+  if (
+
+    (currentBoard.one === turnTracker &&
+    currentBoard.two === turnTracker &&
+    currentBoard.three === turnTracker) ||
+
+    (currentBoard.four === turnTracker &&
+    currentBoard.five === turnTracker &&
+    currentBoard.six === turnTracker) ||
+
+    (currentBoard.seven === turnTracker &&
+    currentBoard.eight === turnTracker &&
+    currentBoard.nine === turnTracker) ||
+
+    (currentBoard.one === turnTracker &&
+    currentBoard.four === turnTracker &&
+    currentBoard.seven === turnTracker) ||
+
+    (currentBoard.two === turnTracker &&
+    currentBoard.five === turnTracker &&
+    currentBoard.eight === turnTracker) ||
+
+    (currentBoard.three === turnTracker &&
+    currentBoard.six === turnTracker &&
+    currentBoard.nine === turnTracker) ||
+
+    (currentBoard.one === turnTracker &&
+    currentBoard.five === turnTracker &&
+    currentBoard.nine === turnTracker) ||
+
+    (currentBoard.three === turnTracker &&
+    currentBoard.five === turnTracker &&
+    currentBoard.seven === turnTracker)
+
+  ) {
+    document.getElementById("winner").innerHTML = `Winner: ${turnTracker}!!  Congratulations!`
+    win = true;
   }
 }
